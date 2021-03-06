7c55b8ae8d4bcdd9d29099a87e413c838c19f903
Dmi3ii2018 <dalekseev2017@gmail.com>
2021-03-06 13:42:10 +0500

    Setted start settings

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import cn from "classnames";
+import s from "./App.module.scss";
+
+const App = () => (
+    <div className={cn(s.header)}>
+        This is App component
+    </div>
+)
+
+export default App
