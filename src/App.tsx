596c8f6fb500e13c608ded559d0ee313f5369e24
Dmi3ii2018 <dalekseev2017@gmail.com>
2021-03-09 21:44:34 +0500

    Added code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Pokedex from './pages/Pokedex';
-import Pokemon from './pages/Pokemon';
-import Ability from './pages/Ability';
+
+const Pokedex = lazy(() => import("./pages/Pokedex"));
+const Pokemon = lazy(() => import("./pages/Pokemon"));
+const Ability = lazy(() => import("./pages/Ability"));
 
 const App = () => (
   <div>
+    <Suspense fallback={
+      <div>
+        Loading...
+      </div>
+    }>
     <Switch>
       <Route path="/" exact render={() => <Pokedex />} />
       <Route path="/pokemon/:id/" exact render={() => <Pokemon />} />
       <Route path="/ability/:name/" render={() => <Ability />} />
-    </Switch>
+      </Switch>
+      </Suspense>
   </div>
 );
 
